f2e2a8a848121c2fb82827aada0d699e0caed6ad
mengjianan20 <1097372359@qq.com>
2020-08-08 18:01:06 +0800

    这是第一次提交STS项目文件

---
+++ src/main/java/com/oracle/git/gitdemo/GitTest.java
@@ -0,0 +1,10 @@
+package com.oracle.git.gitdemo;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("这是STS项目");
+	}
+
+}
